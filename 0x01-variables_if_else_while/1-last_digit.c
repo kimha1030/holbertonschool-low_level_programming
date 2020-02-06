34c599feb6064385fa3fcd136417faaae7dc837b
kimha1030 <1478@holbertonschool.com>
2020-02-06 19:19:24 +0000

    Repository for if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,16 +8,17 @@
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int x = n % 10;
+	int n, x;
 	char str1[] = "Last digit of";
 	char str2[] = "is";
 	char str3[] = "and is greater than 5\n";
 	char str4[] = "and is 0\n";
 	char str5[] = "and is less than 6 and not 0\n";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	x = n % 10;
 
 	if (x > 5)
 	printf("%s %d %s %d %s", str1, n, str2, x, str3);
