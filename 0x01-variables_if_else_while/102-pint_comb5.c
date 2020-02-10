2fe00b78d7eb49e946e79da83a76d5f4d06b9ef7
kimha1030 <1478@holbertonschool.com>
2020-02-10 01:16:12 +0000

    If Else While

---
+++ 0x01-variables_if_else_while/102-pint_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+		{	
+			for (c = '0' ; c <= '9' ; c++)
+			{
+				for (d = '1' ; d <= '9' ; d++)
+				{ 	
+					if (b < d && a <= c)
+                        		{
+					putchar(a);                                                                        putchar(b);                                                                        putchar(' ');
+					putchar(c);
+					putchar(d);
+
+					if ( a <= '9' && b < '8')
+                        		putchar(',');
+                        		putchar(' ');
+					}
+				}
+			}	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
