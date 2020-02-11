169df0416aa8ce1b06faec436ff08fc4151aa7a9
kimha1030 <1478@holbertonschool.com>
2020-02-11 19:51:50 +0000

    Function, nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-	char s[] = "Holberton";
-	int i;
-
-	for (i = 0 ; i <= 8 ; i++)
-	{
-		_putchar(s[i]);
-	}
+	_putchar('H');
+	_putchar('o'); 
+	_putchar('l'); 
+	_putchar('b'); 
+	_putchar('e'); 
+	_putchar('r'); 
+	_putchar('t'); 
+	_putchar('o');
+        _putchar('n');
 	_putchar('\n');
 	return (0);
 }
