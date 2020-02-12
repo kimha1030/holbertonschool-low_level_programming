1faee22c830cecc228ee2be9423bb2e2ee088fc3
kimha1030 <1478@holbertonschool.com>
2020-02-12 22:21:39 +0000

    Loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 */
 void print_to_98(int n)
 {
-	int i; 
+	int i;
 
 	if (n <= 98)
 	{
@@ -29,7 +29,7 @@ void print_to_98(int n)
 		for (i = n ; i >= 98 ; i--)
 		{
 			if (i != 98)
-                        {
+			{
 				printf("%i, ", i);
 			}
 			else
