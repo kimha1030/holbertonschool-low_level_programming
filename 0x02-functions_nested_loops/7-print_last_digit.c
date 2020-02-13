6eabf9758214e903644506f5cb661caf5f15126a
kimha1030 <1478@holbertonschool.com>
2020-02-13 02:50:47 +0000

    Loop

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- *print_last_digit - print last digit
- *@n: the integer print
- *Return: n
+ * print_last_digit - last digit
+ * @n: the integer print
+ * Return: the variable digit
  */
 int print_last_digit(int n)
 {
+	n = n % 10;
 	if (n < 0)
 	{
-		n = n * -1;
+		n = (n * (-1));
 	}
-	n = n % 10;
 	_putchar(n + '0');
 	return (n);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,7 +8,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
-int print_last_digit(int);
+int print_last_digit(int n);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
