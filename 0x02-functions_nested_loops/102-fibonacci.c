2db53843e4adedcf1204095972cf57259925521f
kimha1030 <1478@holbertonschool.com>
2020-02-13 04:03:15 +0000

    Nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @a: long
+ * @b: long
+ * @c: long
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long i, a = 0, b = 1, c = 1;
+	
+	for (i = 1 ; i <= 50 ; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (i == 50)
+			{
+				printf("%ld\n", c);
+			}
+			else
+			{
+				printf("%ld, ", c);
+			}
+	}
+	return (0);
+}
