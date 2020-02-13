eb195a2ac8c4788c58039fa4d36090e9778f3cd7
kimha1030 <1478@holbertonschool.com>
2020-02-13 22:07:49 +0000

    Function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,26 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			if (i != 1)
+			{
+				printf(" %i", i);
+			}
+			else
+			{	
+				printf("%i", i);
+			}
 		}
 	}
 	printf("\n");
