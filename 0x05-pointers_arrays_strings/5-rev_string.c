c200a5da67153c8f27f8169b89d1d213e8f7b948
kimha1030 <1478@holbertonschool.com>
2020-02-18 17:39:55 +0000

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,5 @@
 **/
 void rev_string(char *s)
 {
-	char aux;
-	int i, h, cont = 0;
 
-	aux = s[0];
-	_putchar(aux);
-/**:
-	for (i = 0 ; aux != '\0' ; i++)
-	{
-		cont++;
-		_putchar(cont);
-	}
-
-	for (h = aux - 1 ; h <= 0 ; h--)
-	{
-		cont--;
-		s[0] = s[cont];
-	}
-**/
-}
+},
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,12 @@ void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0 ; str[i] != 0 ; i++)
 	{
-		_putchar(str[i]);
-		i =  i + 2;
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
