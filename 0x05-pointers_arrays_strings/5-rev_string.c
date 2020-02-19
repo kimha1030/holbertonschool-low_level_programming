b8dcf46fa7840a691bc026bc1cc7cb76b79f7ba3
kimha1030 <1478@holbertonschool.com>
2020-02-19 00:49:18 +0000

    pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,25 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * rev_string - Prints a string in reverse.
- * @s: character.
- * Return: Always 0.
-**/
+ * rev_string - Reverse a string.
+ * @s: string.
+ * Return: void.
+ */
+
 void rev_string(char *s)
 {
+	char aux[1000];
+	int c, x, y = 0, z = 0;
 
-},
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		aux[x] = s[x];
+		y++;
+	}
+	for (c = y - 1; c >= 0; c--)
+	{
+		s[c] = aux[z];
+		z++;
+	}
+}
