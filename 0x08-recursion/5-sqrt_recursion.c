c7d3feb0c586d1564791114db6a2f212ec4bd10d
kimha1030 <1478@holbertonschool.com>
2020-02-26 22:55:54 +0000

    pointers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer.
+ * Return: x.
+**/
+int _sqrt_recursion(int n)
+{
+	int a = 0;
+
+	if (a * a = n)
+	{
+		return (a);
+	}
+	else
+	{
+		_sqrt_recursion(n);
+	}
+}
