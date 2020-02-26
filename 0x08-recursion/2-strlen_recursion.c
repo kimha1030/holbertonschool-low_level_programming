2ddd69209eda85d4e63f4c8933ef17f5ca179825
kimha1030 <1478@holbertonschool.com>
2020-02-26 22:54:04 +0000

    pointers

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: string
+ * Return: n.
+**/
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * factorial - function that returns the factorial of a number.
+ * @n: integer
+ * Return: n.
+**/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+	return (n * factorial(n - 1));
+	}
+}
