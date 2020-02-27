a436ee1193465b6ec48c0a7c09a8e2a0867c6443
kimha1030 <1478@holbertonschool.com>
2020-02-27 05:00:40 +0000

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,45 @@
 #include "holberton.h"
 /**
- * is_prime_number - function prints 1 if the number is prime..
+ * prime - define number
  * @n: integer.
- * Return: x.
+ * @i: integer.
+ * Return: int.
 **/
-int is_prime_number(int n)
-	
-	int a;
 
-	if ((a * a) % n == 0)
+int prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, (i - 1)));
+	}
+}
+
+/**
+ * is_prime_number - function prints 1 if the number is prime.
+ * @n: integer.
+ * Return: int.
+**/
+
+int is_prime_number(int n)
+{
+	int result;
+
+	result = prime(n, n / 2);
+	if (result == 1)
 	{
 		return (1);
 	}
 	else
 	{
-		is_prime_number(n);
+		return (-1);
 	}
 	return (0);
 }
