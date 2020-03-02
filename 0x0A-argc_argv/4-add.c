1b573f7fdb12f4e23401ec02c15b010b4829b086
kimha1030 <1478@holbertonschool.com>
2020-03-02 04:56:28 +0000

    Arguments for command line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -35,41 +35,4 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	}
 	return (0);
-}#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-/**
- * main - function that adds positive numbers.
- * @argc: number strings in argv
- * @argv: array of command-line argument
- * Return: Always 0.
- */
-
-int main(int argc, char *argv[])
-{
-	int sum = 0;
-	int i;
-
-	if (argc >= 2)
-	{
-		for (i = 1 ; argv[i] != 0 ; i++)
-		{
-			if (!isdigit(*(argv[i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum = sum + atoi(argv[i]);
-			}
-		}
-		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("0\n");
-	}
-	return (0);
 }
