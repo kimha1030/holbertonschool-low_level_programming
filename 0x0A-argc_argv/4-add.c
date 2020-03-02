56ac274ef5caa8a194623410f1aba26233fb7462
kimha1030 <1478@holbertonschool.com>
2020-03-02 17:30:34 -0500

    Arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+/**
+ *_isdigit - function that return 1 or 0 depends is number
+ * @x: digit
+ * Return: digit
+ */
+
+int _isdigit(char *x)
+{
+	while (*x != '\0')
+	{
+		if (*x < 0 || *x > '9')
+		{
+			return (0);
+		}
+		x++;
+	}
+	return (1);
+
+}
 
 /**
- * main - function that adds positive numbers.
- * @argc: number strings in argv
- * @argv: array of command-line argument
- * Return: Always 0.
+ * main - function that adds 2 numbers
+ * @argc: The number of command line arguments
+ * @argv: Array that contain the command line arguments
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int i;
+	int x, sum = 0;
 
-	if (argc >= 2)
-	{
-		for (i = 1 ; argv[i] != 0 ; i++)
-		{
-			if (!isdigit((int)*(argv[i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum = sum + atoi(argv[i]);
-			}
-		}
-		printf("%d\n", sum);
-	}
-	else
+	if (argc == 0)
 	{
 		printf("0\n");
 	}
+	for (x = 1; x < argc ; x++)
+	{
+		if (_isdigit(argv[x]))
+			sum += atoi(argv[x]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
 	return (0);
 }
