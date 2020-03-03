f39068c12ec36a1f4d548798d6ac6df4bd2d1bb8
kimha1030 <1478@holbertonschool.com>
2020-03-03 16:56:43 -0500

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 
 	char *n;
 
-	if ((s1 == NULL) || (s2 == NULL))
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[x] != '\0')
 		x++;
 	while (s2[y] != '\0')
@@ -30,6 +32,6 @@ char *str_concat(char *s1, char *s2)
 		n[z] = s2[t];
 		z++;
 	}
-		n[z] = '\0';
+	n[x + y] = '\0';
 	return (n);
 }
