ba618854afe22fdd8234e61c7c8f1c2c5370444d
kimha1030 <1478@holbertonschool.com>
2020-03-11 14:15:13 -0400

    pointer to function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
-
 /**
- * print_name : function that prints a name.
+ * print_name - function that prints a name.
  * @name: name of the person
  * @f: pointer
  * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != 0 && f != 0)
