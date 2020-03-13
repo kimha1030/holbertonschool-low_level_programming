2af63c8ffe3083744146bdfdd12eee62d89ef3b8
kimha1030 <1478@holbertonschool.com>
2020-03-13 16:57:47 -0400

    Repository for variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - unction that prints numbers, followed by a new line
+ * @separator: const char.
+ * @n: const unsigned int
+ * Return: void
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		if (separator != NULL)
+		printf("%d", va_arg(list, const unsigned int));
+
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	va_end(list);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings
+ * @separator: const char.
+ * @n: const unsigned int
+ * Return: void
+ **/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *p;
+
+	va_start(list, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		p = va_arg(list, char *);
+		if (p == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", p);
+		}
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+		va_end(list);
+		printf("\n");
+}
