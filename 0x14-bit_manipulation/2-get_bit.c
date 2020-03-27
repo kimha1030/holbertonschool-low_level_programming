f5c80292a483ef3fe01c4a9dfcc1d5fb11753322
kimha1030 <1478@holbertonschool.com>
2020-03-27 00:11:09 -0500

    Task 4

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,6 +3,7 @@
 /**
  * get_bit - returns the value of a bit at a given index.
  * @n: unsigned long int
+ * @index: unsigned int
  * Return: value or -1 if there is an error.
  */
 int get_bit(unsigned long int n, unsigned int index)
