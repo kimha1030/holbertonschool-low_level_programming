282656ae9831363b0a346168a669de7b80e84d84
kimha1030 <1478@holbertonschool.com>
2020-04-01 22:52:54 -0500

    modification

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
-* main - main function
-* Return: 0
-*/
+ * main - main function
+ * Return: 0
+ */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
