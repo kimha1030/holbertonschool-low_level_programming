ac49dea391dcec3826a46ec82baee796c66cc3c4
kimha1030 <1478@holbertonschool.com>
2020-05-29 09:43:25 -0500

    Update task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -22,6 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			copy = strdup(ht->array[i]->value);
+			return (copy);
 		}
 		else
 		{
