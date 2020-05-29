dfea37872c32b854913f0ff61a82fcd58da922ec
kimha1030 <1478@holbertonschool.com>
2020-05-29 00:41:49 -0500

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function that retrieves a value associated with a key.
+ * @ht: hash table you want to add or update the key/value
+ * @key: key
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+	char *copy = NULL;
+
+	if (ht == NULL)
+		return (NULL);
+	if (key == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			copy = strdup(ht->array[i]->value);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (copy);
+}
