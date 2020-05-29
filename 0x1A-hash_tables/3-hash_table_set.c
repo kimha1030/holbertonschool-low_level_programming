104b07a7bac658e136c3c80ae2372962373063e6
kimha1030 <1478@holbertonschool.com>
2020-05-29 14:50:30 -0500

    Update Tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
+			if (tmp->value != NULL)
+				free(tmp->value);
 			tmp->value = copy;
 			return (1);
 		}
