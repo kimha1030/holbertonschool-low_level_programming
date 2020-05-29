c40beb95fedd1abd74749bf12e6f2e37b8c10497
kimha1030 <1478@holbertonschool.com>
2020-05-29 14:41:46 -0500

    Update task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,19 +6,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 	hash_node_t *aux;
 
-	while (ht->array[i])
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
 	{
-		aux = ht->array[i];
-		aux = aux->next;
-		free(aux->key);
-		free(aux->value);
-		free(aux);
+		while (ht->array[i])
+		{
+			aux = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = aux;
+		}
 	}
 	free(ht->array);
-	ht->array = NULL;
 	free(ht);
-	ht = NULL;
 }
